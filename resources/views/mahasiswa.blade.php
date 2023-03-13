11c4977615b6d761d799275c3384ea1a220340a2
vermi <mheri613@gmail.com>
2023-03-14 00:13:46 +0700

    Add files via upload

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -3,22 +3,30 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Daftar Mahasiswa</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <title>Document</title>
 </head>
 
 <body>
-    <h1>Daftar Mahasiswa</h1>
-    <ol>
-        <li><?php echo $mahasiswa01; ?></li>
-        <li><?php echo $mahasiswa02; ?></li>
-        <li><?php echo $mahasiswa03; ?></li>
-        <li><?php echo $mahasiswa04; ?></li>
-    </ol>
+
+    <div class="container text-center mt-3 pt-3 bg-white">
+        <h1 class="bg-dark px-3 py-1 text-white d-inline-block">{{$nama}}</h1>
+        <br>
+        {{--
+@foreach ($nilai as $val)
+@if ($val < 50)
+@break
+@endif
+<div class="alert alert-success d-inline-block">
+{{$val}}
+    </div>
+    @endforeach
+    --}}
+    </div>
+
 </body>
-</ol>
-<div>
-    Copyright &copy; <?php echo date("Y"); ?> Program Studi Teknik Informatika - PNL
-</div>
-</body>
+
+</html>
